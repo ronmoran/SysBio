4f5d08407dd4e7296b625a10c7661d238f5966b6
NitzanS <nitzanshkali@gmail.com>
2021-12-05 03:09:00 +0200

    updated plot func for q4

--- ex2/q1.py
+++ ex2/q1.py
@@ -3,6 +3,8 @@ import plotly.express as px
 import pandas as pd
 import os
 from functools import partial
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 DT = 1e-3
 TOTAL_TIME = 2.5
@@ -72,7 +74,7 @@ def plot_rate_balance(degradation_rate: float, max_rate: float, kd: float, hill_
             "value": "Rate",
             "variable": "Regulation"
         })
-    plt.write_image(os.path.join(PLOT_PATH, fig_name), width=1280, height=720)
+    # plt.write_image(os.path.join(PLOT_PATH, fig_name), width=1280, height=720)
     plt.show()
 
 
@@ -86,36 +88,35 @@ def plot_concentrations(data, variable, fig_name, add_half_concentration=True):
                   }
                   )
     if add_half_concentration:
-        plt.add_hline(y=data.iloc[-1][0] / 2, line_dash="dot", annotation_text="steady-state / 2")
+        plt.add_hline(y=data.iloc[-1][0] / 2, line_dash="dot", annotation_text="steady-state / 2", opacity=0.5)
         half_val = data.iloc[-1] / 2
         half_times = np.abs(data - half_val).idxmin(axis=0)
         for half_time in half_times:
-            plt.add_vline(x=half_time, line_dash="dot", line_color="LightBlue")
+            plt.add_vline(x=half_time, line_dash="dot", opacity=0.2, annotation_text="t 1/2",
+                          annotation_position='bottom right')
     plt.update_xaxes(showticklabels=False)
     plt.show()
     plt.write_image(os.path.join(PLOT_PATH, fig_name), width=1280, height=720)
 
 
-def plot_concentrations_q4(data, fig_name, title, **kwargs):
-    plt = px.line(data,
-                  title=title,
-                  labels={
-                      "index": "Time",
-                      "value": "Concentration",
-                      "variable": "Concentration"
-                  }
-                  )
-    plt.for_each_trace(_set_color)
-    plt.update_xaxes(showticklabels=False)
-    plt.update_yaxes(showticklabels=False)
+def plot_stacked_q4(data, fig_name, title, y_axis_titles, **kwargs):
+    fig = make_subplots(rows=data.shape[0], cols=1, shared_xaxes=True, vertical_spacing=0.01)
+    for i, dat in enumerate(data):
+        fig.add_trace(go.Line(y=data[i]), row=i+1, col=1)
+        fig.update_yaxes(title_text=y_axis_titles[i] + " con.", row=i+1, col=1)
     if "hlines" in kwargs:
         for attrs in kwargs["hlines"]:
-            plt.add_hline(**attrs)
+            fig.add_hline(**attrs, annotation_position="top left", line_width=0.7, line_dash="dot")
     if "vlines" in kwargs:
         for attrs in kwargs["vlines"]:
-            plt.add_vline(**attrs, line_color="LightBlue")
-    plt.show()
-    plt.write_image(os.path.join(PLOT_PATH, fig_name), width=1280, height=720)
+            fig.add_vline(**attrs, line_width=0.3, line_dash="dot")
+    fig.update_layout(title_text=title)
+    fig.update_xaxes(title_text='Time', row=data.shape[0])
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False)
+    fig.update_layout(showlegend=False)
+    fig.show()
+    fig.write_image(os.path.join(PLOT_PATH, fig_name), width=1280, height=720)
 
 
 def _set_color(shape):
@@ -181,12 +182,12 @@ def q4a(kyz, y_max_rate, y_deg_rate, z_max_rate, z_deg_rate, sig_start_time, sig
     y = _create_reg_data_series(signal_time_vals, y_max_rate, y_deg_rate, dt)
     z_prod_time_vals = ((y > kyz) & signal_time_vals)
     z = _create_reg_data_series(z_prod_time_vals, z_max_rate, z_deg_rate, dt)
-    hlines = [dict(y=kyz, line_dash="dash", annotation_text="kyz", annotation_position="top left")]
-    vlines = [dict(x=z_prod_time_vals.argmax(), line_dash="dot")]
+    hlines = [dict(y=kyz, row=2, annotation_text="$K_{YZ}$")]
+    vlines = [dict(x=z_prod_time_vals.argmax())]
     signal = np.where(signal_time_vals, np.max(np.hstack((y, z))) * 1.1, 0.)
-    data = pd.DataFrame(np.vstack((y, z, signal)).T, columns=["Y", "Z", "signal"])
-    plot_concentrations_q4(data, "q4a.png", "X Saturated, AND Gate for Y Level and Signal",
-                           hlines=hlines, vlines=vlines)
+    data = np.array([signal, y, z])
+    plot_stacked_q4(data, "q4a.png", "X Saturated, AND Gate for Y Level and Signal",
+                    y_axis_titles=["signal = X", "Y", "Z"], hlines=hlines, vlines=vlines)
 
 
 def q4b(kyz, kxy, kxz, x_max_rate, x_deg_rate, y_max_rate, y_deg_rate, z_max_rate, z_deg_rate, sig_start_time,
@@ -199,17 +200,17 @@ def q4b(kyz, kxy, kxz, x_max_rate, x_deg_rate, y_max_rate, y_deg_rate, z_max_rat
     y = _create_reg_data_series(y_prd_time, y_max_rate, y_deg_rate, dt)
     z_prod_time_vals = ((y > kyz) & (x > kxz))
     z = _create_reg_data_series(z_prod_time_vals, z_max_rate, z_deg_rate, dt)
-    hlines = [dict(y=kyz, line_dash="dash", annotation_text="kyz", annotation_position="top left"),
-              dict(y=kxy, line_dash="dash", annotation_text="kxy", annotation_position="top left"),
-              dict(y=kxz, line_dash="dash", annotation_text="kxz", annotation_position="top left")]
-    vlines = [dict(x=np.where(y == 0., np.inf, y).argmin(), line_dash="dot"),
-              dict(x=np.where(z == 0., np.inf, z).argmin(), line_dash="dot"),
-              dict(x=y.argmax(), line_dash="dot"),
-              dict(x=z.argmax(), line_dash="dot")]
+    hlines = [dict(y=kyz, row=3, annotation_text="$K_{YZ}$", ),
+              dict(y=kxy, row=2,  annotation_text="$K_{XY}$"),
+              dict(y=kxz, row=2, annotation_text="$K_{XZ}$")]
+    vlines = [dict(x=np.where(y == 0., np.inf, y).argmin()),
+              dict(x=np.where(z == 0., np.inf, z).argmin()),
+              dict(x=y.argmax()),
+              dict(x=z.argmax())]
     signal = np.where(signal_time_vals, np.max(np.hstack((x, y, z))) * 1.1, 0.)
-    data = pd.DataFrame(np.vstack((x, y, z, signal)).T, columns=["X", "Y", "Z", "signal"])
-    plot_concentrations_q4(data, "q4b.png", "X Buildup Upon Signal, AND Gate",
-                           hlines=hlines, vlines=vlines)
+    data = np.array([signal, x, y, z])
+    plot_stacked_q4(data, "q4b.png", "X Buildup Upon Signal, AND Gate", y_axis_titles=["signal", "X", "Y", "Z"],
+                    hlines=hlines, vlines=vlines)
 
 
 def q4c(kyz, y_max_rate, y_deg_rate, z_max_rate, z_deg_rate, sig_start_time, sig_stop_time, total_time, dt):
@@ -219,12 +220,12 @@ def q4c(kyz, y_max_rate, y_deg_rate, z_max_rate, z_deg_rate, sig_start_time, sig
     y = _create_reg_data_series(signal_time_vals, y_max_rate, y_deg_rate, dt)
     z_prod_time_vals = ((y > kyz) | signal_time_vals)
     z = _create_reg_data_series(z_prod_time_vals, z_max_rate, z_deg_rate, dt)
-    hlines = [dict(y=kyz, line_dash="dash", annotation_text="kyz", annotation_position="top left")]
-    vlines = [dict(x=z.argmax(), line_dash="dot")]
+    hlines = [dict(y=kyz, row=2, annotation_text="$K_{YZ}$")]
+    vlines = [dict(x=z.argmax())]
     signal = np.where(signal_time_vals, np.max(np.hstack((y, z))) * 1.1, 0.)
-    data = pd.DataFrame(np.vstack((y, z, signal)).T, columns=["Y", "Z", "signal"])
-    plot_concentrations_q4(data, "q4c.png", "Saturated X Activated Upon signal, OR gate",
-                           hlines=hlines, vlines=vlines)
+    data = np.array([signal, y, z])
+    plot_stacked_q4(data, "q4c.png", "Saturated X Activated Upon signal, OR gate",
+                    y_axis_titles=["signal = X", "Y", "Z"], hlines=hlines, vlines=vlines)
 
 
 def q4d(kyz, kxy, kxz, x_max_rate, x_deg_rate, y_max_rate, y_deg_rate, z_max_rate, z_deg_rate, sig_start_time,
@@ -239,14 +240,14 @@ def q4d(kyz, kxy, kxz, x_max_rate, x_deg_rate, y_max_rate, y_deg_rate, z_max_rat
     z = _create_reg_data_series(z_prod_time_vals, z_max_rate, z_deg_rate, dt)
     z_changes = ((z_prod_time_vals ^ np.roll(z_prod_time_vals, 1))[1:-1]).nonzero()[0]
     y_changes = ((y_prd_time ^ np.roll(y_prd_time, 1))[1:-1]).nonzero()[0]
-    hlines = [dict(y=kyz, line_dash="dash", annotation_text="kyz", annotation_position="top left"),
-              dict(y=kxy, line_dash="dash", annotation_text="kxy", annotation_position="top left"),
-              dict(y=kxz, line_dash="dash", annotation_text="kxz", annotation_position="top left")]
-    vlines = [dict(x=change, line_dash="dot") for change in np.hstack((z_changes, y_changes))]
+    hlines = [dict(y=kyz, row=3, annotation_text="$K_{YZ}$"),
+              dict(y=kxy, row=2, annotation_text="$K_{XY}$"),
+              dict(y=kxz, row=2, annotation_text="$K_{XZ}$")]
+    vlines = [dict(x=change) for change in np.hstack((z_changes, y_changes))]
     signal = np.where(signal_time_vals, np.max(np.hstack((x, y, z))) * 1.1, 0.)
-    data = pd.DataFrame(np.vstack((x, y, z, signal)).T, columns=["X", "Y", "Z", "signal"])
-    plot_concentrations_q4(data, "q4d.png", "Incoherent Type 3, X Buildup Upon Signal",
-                           hlines=hlines, vlines=vlines)
+    data = np.array([signal, x, y, z])
+    plot_stacked_q4(data, "q4d.png", "Incoherent Type 3, X Buildup Upon Signal",
+                    y_axis_titles=["signal", "X", "Y", "Z"], hlines=hlines, vlines=vlines)
 
 
 def q4e(kyz, kxy, kxz, x_max_rate, x_deg_rate, hill_coef, kd, y_max_rate, y_deg_rate, z_max_rate, z_deg_rate,
@@ -261,14 +262,14 @@ def q4e(kyz, kxy, kxz, x_max_rate, x_deg_rate, hill_coef, kd, y_max_rate, y_deg_
     z = _create_reg_data_series(z_prod_time_vals, z_max_rate, z_deg_rate, dt, z_max_rate / z_deg_rate)
     z_changes = ((z_prod_time_vals ^ np.roll(z_prod_time_vals, 1))[1:-1]).nonzero()[0]
     y_changes = ((y_prd_time ^ np.roll(y_prd_time, 1))[1:-1]).nonzero()[0]
-    hlines = [dict(y=kyz, line_dash="dash", annotation_text="kyz", annotation_position="top left"),
-              dict(y=kxy, line_dash="dash", annotation_text="kxy", annotation_position="top left"),
-              dict(y=kxz, line_dash="dash", annotation_text="kxz", annotation_position="top left")]
-    vlines = [dict(x=change, line_dash="dot") for change in np.hstack((z_changes, y_changes))]
+    hlines = [dict(y=kyz, row=3, annotation_text="$K_{YZ}$"),
+              dict(y=kxy, row=2, annotation_text="$K_{XY}$"),
+              dict(y=kxz, row=2, annotation_text="$K_{XZ}$")]
+    vlines = [dict(x=change) for change in np.hstack((z_changes, y_changes))]
     signal = np.where(signal_time_vals, np.max(np.hstack((x, y, z))) * 1.1, 0.)
-    data = pd.DataFrame(np.vstack((x, y, z, signal)).T, columns=["X", "Y", "Z", "signal"])
-    plot_concentrations_q4(data, "q4e.png", "Coherent Type 3 With Negative Auto-Regulation on X",
-                           hlines=hlines, vlines=vlines)
+    data = np.array([signal, x, y, z])
+    plot_stacked_q4(data, "q4e.png", "Coherent Type 3 With Negative Auto-Regulation on X",
+                    y_axis_titles=["signal", "X", "Y", "Z"], hlines=hlines, vlines=vlines)
 
 
 if __name__ == "__main__":
@@ -279,11 +280,11 @@ if __name__ == "__main__":
     kyz_4a = 0.5
     kxy_4b = 0.4
     kxz_4b = 0.65
-    # q1a(kd, hill_coef, max_rate, degradation_rate)
-    # q1b(kd, max_rate, degradation_rate)
-    # q4a(kyz_4a, max_rate, degradation_rate, max_rate, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
+    q1a(kd, hill_coef, max_rate, degradation_rate)
+    q1b(kd, max_rate, degradation_rate)
+    q4a(kyz_4a, max_rate, degradation_rate, max_rate, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
     q4b(kyz_4a * 1.2, kxy_4b, kxz_4b / 3, max_rate / 1.5, degradation_rate * 2, max_rate / 2, degradation_rate * 0.8, max_rate/ 1.3, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
-    # q4c(kyz_4a, max_rate, degradation_rate * 1.5, max_rate, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
-    # q4d(kyz_4a / 2, kxy_4b / 4, kxz_4b - 0.2, max_rate / 1.5, degradation_rate * 2, max_rate, degradation_rate * 1.5, max_rate, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
-    # q4e(kyz_4a, kxy_4b, kxz_4b, max_rate * 1.5, degradation_rate * 2, hill_coef, kd, max_rate / 1.5, degradation_rate,
-    #     max_rate / 3, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
+    q4c(kyz_4a, max_rate, degradation_rate * 1.5, max_rate, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
+    q4d(kyz_4a / 2, kxy_4b / 4, kxz_4b - 0.2, max_rate / 1.5, degradation_rate * 2, max_rate, degradation_rate * 1.5, max_rate, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
+    q4e(kyz_4a, kxy_4b, kxz_4b, max_rate * 1.5, degradation_rate * 2, hill_coef, kd, max_rate / 1.5, degradation_rate,
+         max_rate / 3, degradation_rate, 0.2, 1.2, TOTAL_TIME, DT)
