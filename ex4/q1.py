1a21914e19be02b15ec5c8c4e20d298aba9c37b4
NitzanS <nitzanshkali@gmail.com>
2022-02-24 02:28:08 +0200

    Finished q1.

--- ex4/q1.py
+++ ex4/q1.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import optimize
-import seaborn as sns
 
+SAVE = False
 
 def question1():
     experiment_time = 180  # minutes
@@ -68,11 +68,13 @@ def simulate_experiment_repeats(time_points: np.ndarray, create_rate: float, dec
 def plot_simulations(gaussian_repeats: pd.DataFrame, poisson_repeats: pd.DataFrame):
     gaussian_repeats.plot(title='mRNA Levels of Population (Simulated With Gaussian Noise)',
                           xlabel='Time (minutes)', ylabel='mRNA (# molecules)')
-    plt.savefig("plots/gaussian_simulation.png")
+    if SAVE:
+        plt.savefig("plots/gaussian_simulation.png")
     plt.show()
     poisson_repeats.plot(title='mRNA Levels of Single Cells (Simulated With Poisson Noise)',
                          xlabel='Time (minutes)', ylabel='mRNA (# molecules)')
-    plt.savefig("plots/poisson_simulation.png")
+    if SAVE:
+        plt.savefig("plots/poisson_simulation.png")
     plt.show()
 
 
@@ -93,27 +95,11 @@ def estimate_parameters_from_single_experiments(experiment_repeats: pd.DataFrame
     return params_estimations
 
 
-def plot_all_parameters_estimations(params_estimations: pd.DataFrame, experiment_kind: str, sample_times: float,
-                                    real_create_rate: float, real_decay_rate: float, real_start_amount: float,
-                                    plot_save_name: str):
-    # plot_single_param_estimation(params_estimations["Create Rate"], new_create_rate, 'Creation Rate', 'box')
-    # plot_single_param_estimation(params_estimations["Decay Rate"], decay_rate, 'Decay Rate', 'box')
-    # plot_single_param_estimation(params_estimations["Initial Quantity"], start_amount, 'Initial Quantity', 'box')
-    params_estimations.plot(title=f'Estimations of Parameters Fitted By {experiment_kind} Experiments '
-                                  f'Sampled Every {sample_times} Minutes\n '
-                                  f'{mse_title_helper(params_estimations["Create Rate"], real_create_rate)}                        '
-                                  f'{mse_title_helper(params_estimations["Decay Rate"], real_decay_rate)}                        '
-                                  f'{mse_title_helper(params_estimations["Initial Quantity"], real_start_amount)}',
-                            subplots=True, kind='box', figsize=(15, 5))
-    plt.savefig(f"plots/{plot_save_name}.png")
-    plt.show()
-
-
 def plot_parameters_estimations(params_estimations: pd.DataFrame, experiment_kind: str, sample_times: float,
                                 real_create_rate: float, real_decay_rate: float, real_start_amount: float,
                                 plot_save_name: str):
-    titles = [f'{mse_title_helper(params_estimations["Create Rate"], real_create_rate)}',
-              f'{mse_title_helper(params_estimations["Decay Rate"], real_decay_rate)}']
+    titles = [f'{nrmse_title_helper(params_estimations["Create Rate"], real_create_rate)}',
+              f'{nrmse_title_helper(params_estimations["Decay Rate"], real_decay_rate)}']
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 7))
     plt.suptitle(f"Estimation of Creation and Decay Rate Fitted By {experiment_kind}\nExperiments Sampled Every "
                  f"{sample_times} Minutes")
@@ -123,23 +109,23 @@ def plot_parameters_estimations(params_estimations: pd.DataFrame, experiment_kin
         axes[i].scatter(np.random.normal(1, 0.04, params_estimations[col].values.shape[0]), val, alpha=0.3)
         axes[i].set_title(titles[i])
 
-    plt.savefig(f"plots/{plot_save_name}.png")
+    if SAVE:
+        plt.savefig(f"plots/{plot_save_name}.png")
     plt.show()
 
 
+def nrmse_title_helper(estimators: pd.DataFrame, real_val: float):
+    return f'Real Value Used: {real_val}, NRMSE: {nrmse(estimators, real_val):.5f}'
 
-def mse_title_helper(estimators: pd.DataFrame, real_val: float):
-    return f'Real Value Used: {real_val}, MSE: {mse(estimators, real_val):.5f}'
 
-
-def mse(x: pd.DataFrame, val: float):
-    return ((np.array(x) - val) ** 2).mean()
+def nrmse(x: pd.DataFrame, val: float):
+    return np.sqrt(((np.array(x) - val) ** 2).mean()) / x.std()
 
 
 # todo - delete
 def plot_single_param_estimation(estimators: pd.DataFrame, real_val: float, param_name: str, plot_kind: str):
     estimators.plot(kind=plot_kind, title=f'{param_name} Fit From Single Repeats,\n'
-                                          f'Real Value Used: {real_val}, MSE: {mse(estimators, real_val):.2f}')
+                                          f'Real Value Used: {real_val}, NRMSE: {nrmse(estimators, real_val):.2f}')
     plt.show()
 
 
